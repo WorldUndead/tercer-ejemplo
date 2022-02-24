7dad9e9e3eeaf9b23940047d47f1b6b86b9301c6
WorldUndead <sashadin2000@gmail.com>
2022-02-24 17:23:26 -0300

    tercer ejemplo

---
+++ prOperadoresAritmeticos/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.txtnum1 = New System.Windows.Forms.TextBox
+        Me.txtnum2 = New System.Windows.Forms.TextBox
+        Me.Label3 = New System.Windows.Forms.Label
+        Me.Label4 = New System.Windows.Forms.Label
+        Me.Label5 = New System.Windows.Forms.Label
+        Me.txtsuma = New System.Windows.Forms.TextBox
+        Me.txtresta = New System.Windows.Forms.TextBox
+        Me.txtmult = New System.Windows.Forms.TextBox
+        Me.Label6 = New System.Windows.Forms.Label
+        Me.Label7 = New System.Windows.Forms.Label
+        Me.Label8 = New System.Windows.Forms.Label
+        Me.txtdiv = New System.Windows.Forms.TextBox
+        Me.txtpot = New System.Windows.Forms.TextBox
+        Me.txtraiz = New System.Windows.Forms.TextBox
+        Me.btnnuevo = New System.Windows.Forms.Button
+        Me.btncalcular = New System.Windows.Forms.Button
+        Me.btnsalir = New System.Windows.Forms.Button
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtnum2)
+        Me.GroupBox1.Controls.Add(Me.txtnum1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingresar Datos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtraiz)
+        Me.GroupBox2.Controls.Add(Me.txtpot)
+        Me.GroupBox2.Controls.Add(Me.txtdiv)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.txtmult)
+        Me.GroupBox2.Controls.Add(Me.txtresta)
+        Me.GroupBox2.Controls.Add(Me.txtsuma)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 136)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(297, 87)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Número 1:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Número  2:"
+        '
+        'txtnum1
+        '
+        Me.txtnum1.Location = New System.Drawing.Point(75, 28)
+        Me.txtnum1.Name = "txtnum1"
+        Me.txtnum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum1.TabIndex = 2
+        '
+        'txtnum2
+        '
+        Me.txtnum2.Location = New System.Drawing.Point(75, 63)
+        Me.txtnum2.Name = "txtnum2"
+        Me.txtnum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtnum2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Suma:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 42)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(38, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Resta:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(6, 65)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(74, 13)
+        Me.Label5.TabIndex = 2
+        Me.Label5.Text = "Multiplicación:"
+        '
+        'txtsuma
+        '
+        Me.txtsuma.Location = New System.Drawing.Point(86, 16)
+        Me.txtsuma.Name = "txtsuma"
+        Me.txtsuma.Size = New System.Drawing.Size(65, 20)
+        Me.txtsuma.TabIndex = 3
+        '
+        'txtresta
+        '
+        Me.txtresta.Location = New System.Drawing.Point(86, 39)
+        Me.txtresta.Name = "txtresta"
+        Me.txtresta.Size = New System.Drawing.Size(65, 20)
+        Me.txtresta.TabIndex = 4
+        '
+        'txtmult
+        '
+        Me.txtmult.Location = New System.Drawing.Point(86, 61)
+        Me.txtmult.Name = "txtmult"
+        Me.txtmult.Size = New System.Drawing.Size(65, 20)
+        Me.txtmult.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(153, 19)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "División:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(153, 42)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(52, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Potencia:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(153, 65)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(33, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Raíz:"
+        '
+        'txtdiv
+        '
+        Me.txtdiv.Location = New System.Drawing.Point(211, 16)
+        Me.txtdiv.Name = "txtdiv"
+        Me.txtdiv.Size = New System.Drawing.Size(79, 20)
+        Me.txtdiv.TabIndex = 9
+        '
+        'txtpot
+        '
+        Me.txtpot.Location = New System.Drawing.Point(211, 39)
+        Me.txtpot.Name = "txtpot"
+        Me.txtpot.Size = New System.Drawing.Size(79, 20)
+        Me.txtpot.TabIndex = 10
+        '
+        'txtraiz
+        '
+        Me.txtraiz.Location = New System.Drawing.Point(211, 62)
+        Me.txtraiz.Name = "txtraiz"
+        Me.txtraiz.Size = New System.Drawing.Size(79, 20)
+        Me.txtraiz.TabIndex = 11
+        '
+        'btnnuevo
+        '
+        Me.btnnuevo.Location = New System.Drawing.Point(234, 24)
+        Me.btnnuevo.Name = "btnnuevo"
+        Me.btnnuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnnuevo.TabIndex = 2
+        Me.btnnuevo.Text = "Nuevo"
+        Me.btnnuevo.UseVisualStyleBackColor = True
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(234, 53)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btncalcular.TabIndex = 3
+        Me.btncalcular.Text = "Calcular"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'btnsalir
+        '
+        Me.btnsalir.Location = New System.Drawing.Point(234, 90)
+        Me.btnsalir.Name = "btnsalir"
+        Me.btnsalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnsalir.TabIndex = 4
+        Me.btnsalir.Text = "Salir"
+        Me.btnsalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(321, 235)
+        Me.Controls.Add(Me.btnsalir)
+        Me.Controls.Add(Me.btncalcular)
+        Me.Controls.Add(Me.btnnuevo)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = ".:. Operadores Aritmeticos .:."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents txtnum2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtnum1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtraiz As System.Windows.Forms.TextBox
+    Friend WithEvents txtpot As System.Windows.Forms.TextBox
+    Friend WithEvents txtdiv As System.Windows.Forms.TextBox
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents txtmult As System.Windows.Forms.TextBox
+    Friend WithEvents txtresta As System.Windows.Forms.TextBox
+    Friend WithEvents txtsuma As System.Windows.Forms.TextBox
+    Friend WithEvents btnnuevo As System.Windows.Forms.Button
+    Friend WithEvents btncalcular As System.Windows.Forms.Button
+    Friend WithEvents btnsalir As System.Windows.Forms.Button
+
+End Class
---
+++ prOperadoresAritmeticos/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Private Sub TextBox5_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtmult.TextChanged
+
+    End Sub
+
+    Private Sub btncalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btncalcular.Click
+        'Declaración de variables
+        Dim num1 As Integer
+        Dim num2 As Integer
+        Dim suma, resta, mult As Integer
+        Dim div As Double
+        Dim potencia As Integer
+        Dim raiz As Double
+
+        'Entrada de datos
+
+        num1 = Val(txtnum1.Text)
+        num2 = Val(txtnum2.Text)
+
+        'Proceso
+        suma = num1 + num2
+        resta = num1 - num2
+        mult = num1 * num2
+        div = num1 / num2
+        potencia = num1 ^ num2
+        raiz = num1 ^ (1 / num2)
+
+        'Salida de Información
+        txtsuma.Text = suma
+        txtresta.Text = resta
+        txtmult.Text = mult
+        txtdiv.Text = div
+        txtpot.Text = potencia
+        txtraiz.Text = raiz
+
+    End Sub
+
+    Private Sub btnnuevo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnnuevo.Click
+
+        txtnum1.Clear()
+        txtnum2.Clear()
+        txtsuma.Clear()
+        txtresta.Clear()
+        txtmult.Clear()
+        txtdiv.Clear()
+        txtpot.Clear()
+        txtraiz.Clear()
+
+    End Sub
+
+    Private Sub btnsalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnsalir.Click
+        End
+
+    End Sub
+End Class
